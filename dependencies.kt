5fe69feed658493ccfd58f85604b98ef8ee0f48a
Carlos Osorio <hacybeyker@gmail.com>
2023-08-24 10:31:56 -0500

    update dependencies

--- dependencies.kt
+++ dependencies.kt
@@ -1,47 +1,51 @@
 object AppVersion {
-    const val kotlinVersion                     = "1.7.10"
-    const val compileSdkVersion                 = 31
-    const val buildToolsVersion                 = "31.0.0"
-    const val minSdkVersion                     = 23
-    const val targetSdkVersion                  = 31
+
+    const val kotlinVersion                     = "1.9.0"
+
+    const val compileSdkVersion                 = 33
+    const val buildToolsVersion                 = "30.0.3"
+    const val minSdkVersion                     = 24
+    const val targetSdkVersion                  = 33
+
     const val testInstrumentationRunner         = "androidx.test.runner.AndroidJUnitRunner"
 
-    const val coreKtxVersion                    = "1.7.0"
-    const val appCompatVersion                  = "1.4.2"
-    const val materialVersion                   = "1.7.0-alpha02"
+    const val coreKtxVersion                    = "1.10.1"
+    const val appCompatVersion                  = "1.6.1"
+    const val materialVersion                   = "1.9.0"
     const val constraintLayoutVersion           = "2.1.4"
     const val viewPager2Version                 = "1.0.0"
 
-    const val daggerVersion                     = "2.35.1"
-    const val hiltVersion                       = "2.43.1"
-    const val fragmentKtxVersion                = "1.4.0"
+    const val daggerVersion                     = "2.47"
+    const val hiltVersion                       = "2.47"
+    const val fragmentKtxVersion                = "1.6.1"
 
-    const val koinVersion                       = "2.2.3"
+    const val koinVersion                       = "3.4.3"
+    const val koinScopeVersion                  = "2.2.3"
 
     const val retrofitVersion                   = "2.9.0"
     const val converterGsonVersion              = "2.9.0"
-    const val gsonVersion                       = "2.8.6"
-    const val loggingInterceptorVersion         = "4.9.0"
+    const val gsonVersion                       = "2.10.1"
+    const val loggingInterceptorVersion         = "4.11.0"
     const val okHttpJsonMockVersion             = "3.0"
 
-    const val rxJavaVersion                     = "2.2.7"
-    const val rxAndroidVersion                  = "2.1.1"
-    const val adapterRxJavaVersion              = "2.5.0"
+    const val rxJavaVersion                     = "3.1.6"
+    const val rxAndroidVersion                  = "3.0.2"
+    const val adapterRxJavaVersion              = "2.9.0"
 
-    const val lifecycleVersion                  = "2.4.1"
-    const val coroutinesVersion                 = "1.6.0"
+    const val lifecycleVersion                  = "2.6.1"
+    const val coroutinesVersion                 = "1.7.3"
 
-    const val roomVersion                       = "2.4.2"
+    const val roomVersion                       = "2.5.2"
     const val preferencesVersion                = "1.2.0"
 
-    const val chuckerVersion                    = "3.5.0"
+    const val chuckerVersion                    = "4.0.0"
 
-    const val glideVersion                      = "4.12.0"
+    const val glideVersion                      = "4.16.0"
 
-    const val gmsMapsVersion                    = "17.0.1"
-    const val hmsMapsVersion                    = "6.0.0.301"
+    const val gmsMapsVersion                    = "18.1.0"
+    const val hmsMapsVersion                    = "6.11.0.304"
 
-    const val lottieVersion                     = "5.0.3"
+    const val lottieVersion                     = "6.1.0"
 
     const val shimmerFacebookVersion            = "0.5.0"
 }
@@ -63,8 +67,8 @@ object AppDependencies {
     const val fragmentKtx                       = "androidx.fragment:fragment-ktx:${AppVersion.fragmentKtxVersion}"
 
     const val koinCore                          = "io.insert-koin:koin-core:${AppVersion.koinVersion}"
-    const val koinAndroidxScope                 = "io.insert-koin:koin-androidx-scope:${AppVersion.koinVersion}"
-    const val koinAndroidxViewModel             = "io.insert-koin:koin-androidx-viewmodel:${AppVersion.koinVersion}"
+    const val koinAndroidxScope                 = "io.insert-koin:koin-androidx-scope:${AppVersion.koinScopeVersion}"
+    const val koinAndroidxViewModel             = "io.insert-koin:koin-androidx-viewmodel:${AppVersion.koinScopeVersion}"
 
     const val retrofit                          = "com.squareup.retrofit2:retrofit:${AppVersion.retrofitVersion}"
     const val converterGson                     = "com.squareup.retrofit2:converter-gson:${AppVersion.converterGsonVersion}"
@@ -72,9 +76,9 @@ object AppDependencies {
     const val loggingInterceptor                = "com.squareup.okhttp3:logging-interceptor:${AppVersion.loggingInterceptorVersion}"
     const val okHttpJsonMock                    = "com.github.mirrajabi:okhttp-json-mock:${AppVersion.okHttpJsonMockVersion}"
 
-    const val rxJava                            = "io.reactivex.rxjava2:rxjava:${AppVersion.rxJavaVersion}"
-    const val rxAndroid                         = "io.reactivex.rxjava2:rxandroid:${AppVersion.rxAndroidVersion}"
-    const val adapterRxJava                     = "com.squareup.retrofit2:adapter-rxjava2:${AppVersion.adapterRxJavaVersion}"
+    const val rxJava                            = "io.reactivex.rxjava3:rxjava:${AppVersion.rxJavaVersion}"
+    const val rxAndroid                         = "io.reactivex.rxjava3:rxandroid:${AppVersion.rxAndroidVersion}"
+    const val adapterRxJava                     = "com.squareup.retrofit2:adapter-rxjava3:${AppVersion.adapterRxJavaVersion}"
 
     const val lifecycleViewModel                = "androidx.lifecycle:lifecycle-viewmodel-ktx:${AppVersion.lifecycleVersion}"
     const val lifecycleLiveData                 = "androidx.lifecycle:lifecycle-livedata-ktx:${AppVersion.lifecycleVersion}"
@@ -103,35 +107,24 @@ object AppDependencies {
     const val shimmerFacebook                   = "com.facebook.shimmer:shimmer:${AppVersion.shimmerFacebookVersion}"
 }
 
-object ClassPath{
-    const val gradle                            = "com.android.tools.build:gradle:7.2.2"
-    const val kotlinGradlePlugin                = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21"
-    const val hiltAndroidPlugin                 = "com.google.dagger:hilt-android-gradle-plugin:2.43.1"
-    const val jacoco                            = "org.jacoco:org.jacoco.core:0.8.7"
-    const val sonarqubeGradlePlugin             = "org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3"
-    const val secretGradlePlugin                = "com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1"
-    const val googleServices                    = "com.google.gms:google-services:4.3.13"
-}
-
 object TestVersion{
     const val junitVersion                      = "4.13.2"
-    const val mockitoVersion                    = "4.3.1"
-    const val mockitoKotlinVersion              = "4.0.0"
+    const val mockitoVersion                    = "5.5.0"
+    const val mockitoInlineVersion              = "5.2.0"
+    const val mockitoKotlinVersion              = "5.1.0"
     const val extJUnitVersion                   = "1.1.3"
     const val espressoCoreVersion               = "3.4.0"
-    const val mockitoNhaarmanVersion            = "2.2.0"
     const val kotlinCoroutinesTestVersion       = "1.3.2"
-    const val robolectricVersion                = "4.7.3"
-    const val robolectricShadowVersion          = "4.1"
+    const val robolectricVersion                = "4.10.3"
+    const val robolectricShadowVersion          = "4.8.2"
     const val hiltVersion                       = "2.40.5"
     const val hamcrestVersion                   = "2.2"
-    const val coreKtxVersion                    = "1.4.0"
-    const val junitKtxVersion                   = "1.1.3"
-    const val fragmentTestVersion               = "1.4.1"
-    const val archCoreTestVersion               = "2.1.0"
+    const val coreKtxVersion                    = "1.5.0"
+    const val junitKtxVersion                   = "1.1.5"
+    const val fragmentTestVersion               = "1.6.1"
+    const val archCoreTestVersion               = "2.2.0"
     const val espressoContribVersion            = "3.4.0"
-    const val mockWebServerVersion              = "4.9.3"
-    const val mockKVersion                      = "1.12.2"
+    const val mockWebServerVersion              = "4.11.0"
 }
 
 object TestDependencies{
@@ -143,9 +136,8 @@ object TestDependencies{
     const val espressoContrib                   = "androidx.test.espresso:espresso-contrib:${TestVersion.espressoContribVersion}"
 
     const val mockitoCore                       = "org.mockito:mockito-core:${TestVersion.mockitoVersion}"
-    const val mockitoInline                     = "org.mockito:mockito-inline:${TestVersion.mockitoVersion}"
+    const val mockitoInline                     = "org.mockito:mockito-inline:${TestVersion.mockitoInlineVersion}"
     const val mockitoKotlin                     = "org.mockito.kotlin:mockito-kotlin:${TestVersion.mockitoKotlinVersion}"
-    const val mockitoKotlinNhaarman             = "com.nhaarman.mockitokotlin2:mockito-kotlin:${TestVersion.mockitoNhaarmanVersion}"
 
     const val kotlinCoroutines                  = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${TestVersion.kotlinCoroutinesTestVersion}"
     const val hiltAndroid                       = "com.google.dagger:hilt-android-testing:${TestVersion.hiltVersion}"
@@ -160,7 +152,6 @@ object TestDependencies{
     const val fragment                          = "androidx.fragment:fragment-testing:${TestVersion.fragmentTestVersion}"
     const val archCore                          = "androidx.arch.core:core-testing:${TestVersion.archCoreTestVersion}"
     const val mockWebServer                     = "com.squareup.okhttp3:mockwebserver:${TestVersion.mockWebServerVersion}"
-    const val mockK                             = "io.mockk:mockk:${TestVersion.mockKVersion}"
 }
 
 object ValidateVersion{
